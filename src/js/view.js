d429992f2daa18b29fc2e4bf4bf6e7bf785be73a
Kellen James <8484636@ups.com>
2025-05-02 17:29:30 -0400

    feat: scaffold Tooltip view class with production-ready show() method

--- src/js/view.js
+++ src/js/view.js
@@ -1,1 +1,31 @@
-// Query selectors, DOM updates, add/remove classes, etc.
+class Tooltip {
+  constructor() {
+    this.desktopWrapper = document.querySelector('.share-wrapper.desktop');
+    this.desktopButton = document.querySelector('.share-button.desktop');
+
+    this.mobileWrapper = document.querySelector('.share-wrapper.mobile');
+    this.mobileButton = document.querySelector('.share-button.mobile');
+  }
+
+  show() {
+    if (this.desktopButton) {
+      this.desktopButton.setAttribute('aria-expanded', 'true');
+    }
+
+    if (this.mobileButton) {
+      this.mobileButton.setAttribute('aria-expanded', 'true');
+    }
+
+    if (this.desktopWrapper) {
+      this.desktopWrapper.classList.add('is-visible');
+    }
+
+    if (this.mobileWrapper) {
+      this.mobileWrapper.classList.add('is-visible');
+    }
+  }
+
+  hide() {}
+
+  toggle() {}
+}
